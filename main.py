5d0e701f11acd7cb56111abd42499cf1057459e8
pmachere <pierre.macherel@ens-lyon.fr>
2015-11-30 15:59:28 +0100

    evaluate the model on the test set (comparaison between label and prediction not yet implemented)

--- main.py
+++ main.py
@@ -31,17 +31,19 @@ data,Y=lf.loadLabeled("./data/train")
 
 #TODO : corssvalidation
 n_folds = 5
-kf = KFold(len(data), n_folds=n_folds)
-for i, (train_index_array, test_index_array) in enumerate(kf):
-	print("cross validation ({} out of {})".format(i+1, n_folds))
+kf = KFold(len(data), n_folds=n_folds, shuffle=True)
+for folds_index, (train_index_array, test_index_array) in enumerate(kf):
+	print("cross validation ({} out of {})".format(folds_index+1, n_folds))
 	data_train, Y_train = [], []
 	data_test, Y_test = [], []
+	data_test_name = []
 	for train_index in train_index_array:
 		data_train.append(data[train_index])
 		Y_train.append(Y[train_index])
-	for test_index in test_index_array:
+	for j,test_index in enumerate(test_index_array):
 		data_test.append(data[test_index])
 		Y_test.append(Y[test_index])
+		data_test_name.append(str(j) + "({})".format(Y[test_index]))
 
 	print "\t", len(data_train)
 	data_trainRDD=sc.parallelize(data_train,numSlices=16)
@@ -72,6 +74,15 @@ for i, (train_index_array, test_index_array) in enumerate(kf):
 	mb=sc.broadcast(model)
 
 	#TODO : testing of crossvalidation
+	name_text=zip(data_test_name,data_test)
+	#for each doc :(name,text):
+	#apply the model on the vector representation of the text
+	#return the name and the class
+	predictions=sc.parallelize(name_text).map(partial(Predict,dictionary=dict_broad.value,model=mb.value)).collect()
+	output=file('./classifications_test_{}.txt'.format(folds_index+1),'w')
+	for x in predictions:
+		output.write('%s\t%d\n'%x)
+	output.close()
 
 test,names=lf.loadUknown('./data/test')
 name_text=zip(names,test)
